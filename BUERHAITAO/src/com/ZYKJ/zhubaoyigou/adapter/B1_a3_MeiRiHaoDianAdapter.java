c747e2dced923aeece0368a9fc7477daf4532a69
cuishenghu <351088978@qq.com>
2015-11-21 14:00:36 +0800

    csh 2015-11-21

--- BUERHAITAO/src/com/ZYKJ/zhubaoyigou/adapter/B1_a3_MeiRiHaoDianAdapter.java
+++ BUERHAITAO/src/com/ZYKJ/zhubaoyigou/adapter/B1_a3_MeiRiHaoDianAdapter.java
@@ -14,7 +14,10 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.ZYKJ.zhubaoyigou.R;
+import com.ZYKJ.zhubaoyigou.utils.AnimateFirstDisplayListener;
+import com.ZYKJ.zhubaoyigou.utils.ImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 /**
  * @author lss 2015年6月24日 每日好店adapter
@@ -22,6 +25,7 @@ import com.nostra13.universalimageloader.core.ImageLoader;
  */
 public class B1_a3_MeiRiHaoDianAdapter  extends BaseAdapter {
 	private Activity context;
+	private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
 	List<Map<String, String>> data = new ArrayList<Map<String, String>>();
 
 	public B1_a3_MeiRiHaoDianAdapter(Activity context, List<Map<String, String>> data) {
@@ -65,7 +69,9 @@ public class B1_a3_MeiRiHaoDianAdapter  extends BaseAdapter {
 		}else{
 			ViewHolder=(ViewHolder) convertView.getTag();
 		}
-		ImageLoader.getInstance().displayImage((String)data.get(position).get("store_label"), ViewHolder.im_a3_pic);
+		ImageLoader.getInstance().displayImage((String)data.get(position).get("store_label"),  ViewHolder.im_a3_pic, ImageOptions.getOpstion(), animateFirstListener);
+		
+//		ImageLoader.getInstance().displayImage((String)data.get(position).get("store_label"), ViewHolder.im_a3_pic);
 		ViewHolder.tv_a3_storename.setText(data.get(position).get("store_name"));
 		ViewHolder.tv_a3_juli.setText(data.get(position).get("juli"));
 		ViewHolder.comment_rating_bar.setRating(Float.parseFloat(data.get(position).get("store_desccredit")));
